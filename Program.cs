d797c827d316d44fc5ae56db0ba1d4fd2955d038
ShoroukShawky <324243558@sha.edu.eg>
2026-03-11 22:35:23 +0100

    Q2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿namespace OOP06
+﻿using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Claims;
+
+namespace OOP06
 {
     internal class Program
     {
@@ -16,7 +20,24 @@
             // abstraction -> when you tap an app to sent meesages or make a call without knowing how the internal software or processor work, the phone only shows the functions you need
             // encapsulation -> the internal data like system fils are hidden and protected insid the OS ,users can't access or modify them directly 
             #endregion
+            #region Q2
+            // 1- inheritance :
+            // a class can implement multiple interfaces , but can inherit from only one abstract class
 
+            // 2- implementation:
+            // abstract Class can contain both implemented methods and abstract methods , but
+            // interface can contains only method declarations
+
+            // 3- fields and constructors:
+            // abstract calss can have fields and constructors ,but,interface  can't have constructors or normal fields
+
+            // 4- purpose:
+            // interface represents a capability or behavior, abstract class represent an "is-a hierarchy"
+
+            //  When would you choose one over the other? 
+            // use interface when you need loose coupling, extensibility, or multiple inheritance
+            // use abstract class when classes share common code or represent the same hierarchy
+            #endregion
             #endregion
         }
     }
