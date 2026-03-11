ae5745ea9da51613a91b99f50999c7d5690bd0d8
ShoroukShawky <324243558@sha.edu.eg>
2026-03-11 22:16:13 +0100

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            #region part01
+
+            #region Q1
+            // Abstraction is the process of exposing only what the user needs and hiding how it is implemented
+
+            // Abstraction -> focuses on what the object does
+            // Encapsulation -> focuses on protecting the data and controlling access to it
+
+            // real-word example: Samrtphons
+            // abstraction -> when you tap an app to sent meesages or make a call without knowing how the internal software or processor work, the phone only shows the functions you need
+            // encapsulation -> the internal data like system fils are hidden and protected insid the OS ,users can't access or modify them directly 
+            #endregion
+
+            #endregion
         }
     }
 }
